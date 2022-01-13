79bf6fbbd3941e3f2f05be49824a7ed36ce41ee4
Soeun01 <whthdms9@gmail.com>
2022-01-13 19:35:34 +0900

    메뉴 바 수정

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,22 +1,22 @@
-var btn_login = document.querySelector('.btn-primary');
+// var btn_login = document.querySelector('.btn-primary');
 
-btn_login.addEventListener('click', function() {
-    var url = 'http://localhost:58156/Service1.svc/Stu_Login';
-    // 보낼 데이터
-    var data = {
-        id: document.getElementById('email').value,
-        password: document.getElementById('fullname').value
-    };
+// btn_login.addEventListener('click', function() {
+//     var url = 'http://localhost:58156/Service1.svc/Stu_Login';
+//     // 보낼 데이터
+//     var data = {
+//         id: document.getElementById('email').value,
+//         password: document.getElementById('fullname').value
+//     };
 
-    fetch(url, {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'same-origin',   //omit(X), same-origin(cors), include(no-cors)
-            body: data,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error));
-});
+//     fetch(url, {
+//             method: 'POST',
+//             mode: 'cors',
+//             credentials: 'same-origin',   //omit(X), same-origin(cors), include(no-cors)
+//             body: data,
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//         }).then(res => res.json())
+//         .then(response => console.log('Success:', JSON.stringify(response)))
+//         .catch(error => console.error('Error:', error));
+// });

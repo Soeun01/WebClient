bb8ab8b039935221f917ce539fbb75c22c2ca205
Soeun01 <whthdms9@gmail.com>
2022-01-14 17:55:01 +0900

    2.2 도서관 검색 페이지 생성 중
    2022-01-14

--- assets/js/login.js
+++ assets/js/login.js
@@ -20,3 +20,29 @@
 //         .then(response => console.log('Success:', JSON.stringify(response)))
 //         .catch(error => console.error('Error:', error));
 // });
+
+var apiUrl = "http://andrewgodfroyportfolioapi.azurewebsites.net/api/Authentication";
+    //alert(username + "|" + password + "|" + apiUrl);
+    $.ajax({
+        url: apiUrl,
+        type: "POST",
+        data: {
+            username: username,
+            password: password
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            var authenticatedUser = JSON.parse(response);
+            //alert("Data Loaded: " + authenticatedUser);
+            if (onComplete != null) {
+                onComplete(authenticatedUser);
+            }
+        },
+        error: function (xhr, status, error) {
+            //alert(xhr.responseText);
+            if (onComplete != null) {
+                onComplete(xhr.responseText);
+            }
+        }
+    });
